e4fcc52cee3a79c3d39c92ff3bc0e3d6f1043dd9
halvormundal <halvorbmundal@gmail.com>
2023-06-07 23:12:39 +0200

    legg til ./app og ./public

---
+++ app/routes/heihei.tsx
@@ -0,0 +1,7 @@
+export default function Heihei() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Hello sommerstudenter2!</h1>
+    </div>
+  );
+}
