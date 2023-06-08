e4642ab68b4884688c46618b0774938bdb69778a
halvormundal <halvorbmundal@gmail.com>
2023-06-08 14:55:19 +0200

    Fjern heihei.tsx

--- app/routes/heihei.tsx
+++
@@ -1,7 +0,0 @@
-export default function Heihei() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Hello sommerstudenter2!</h1>
-    </div>
-  );
-}
