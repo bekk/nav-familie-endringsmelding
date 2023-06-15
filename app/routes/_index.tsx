f29011ad91a97d2f494477255012de203e58b24a
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-15 15:56:09 +0200

    skal hente tittel-tekst fra Sanity

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,8 @@
 import type { V2_MetaFunction } from '@remix-run/node';
 import { Heading } from '@navikt/ds-react';
 import css from './_index.module.css';
+import { useLoaderData } from '@remix-run/react';
+import { createClient } from '@sanity/client';
 import React from 'react';
 
 export const meta: V2_MetaFunction = () => {
@@ -14,12 +16,27 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
+const sanityKlient = createClient({
+  projectId: 'd8ycstqz',
+  dataset: 'production',
+  apiVersion: '2021-10-21',
+  useCdn: true,
+});
+
+export const loader = async () => {
+  const data = await sanityKlient.fetch('*');
+  console.log('response', data);
+  return { data };
+};
+
 export default function Index() {
+  const { data } = useLoaderData<typeof loader>();
+
   return (
     <div className={`${css.sentrerTekst} ${css.fyllSide}`}>
       <div>
-        <Heading size="xlarge">
-          Hello <span className={css.regnbue}>sommerjobbere</span>!
+        <Heading level="1" size="xlarge">
+          {data ? data[0].nb[0].children[0].text : 'Sanity funker ikke'}
         </Heading>
       </div>
     </div>
