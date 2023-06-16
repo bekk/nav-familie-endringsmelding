b9c03533eff3063f996d340259bbcc400a46c5c0
elisetelje <elise.telje@bekk.no>
2023-06-16 15:32:17 +0200

    Skal printe tekst fra forside i sanity i consolen

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,8 @@ import { useLoaderData } from '@remix-run/react';
 import { createClient } from '@sanity/client';
 import React from 'react';
 import VeilederHilsen from '../komponenter/veilederhilsen/veilederhilsen';
+import { ESanityApiKey, SanityDokument } from '~/typer/sanity/sanity';
+//import { LocaleType, Sprakvelger } from '@navikt/familie-sprakvelger';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -25,20 +27,30 @@ const sanityKlient = createClient({
 });
 
 export const loader = async () => {
-  const data = await sanityKlient.fetch('*');
-  return { data };
+  const forsideTekst = await sanityKlient.fetch<SanityDokument[]>(
+    '*[steg == "FORSIDE"]',
+  );
+  return { forsideTekst };
 };
 
 export default function Index() {
-  const { data } = useLoaderData<typeof loader>();
+  const { forsideTekst } = useLoaderData<typeof loader>();
+
+  const dokumenter: Map<ESanityApiKey, SanityDokument> = new Map();
+  forsideTekst.map(dokument => dokumenter.set(dokument.api_navn, dokument));
+
+  console.log(dokumenter.get(ESanityApiKey.TITTEL));
 
   return (
     <div className={`${css.sentrerTekst} ${css.fyllSide}`}>
-       <div className={`${css.innholdkonteiner}`}>
+      <div className={`${css.innholdkonteiner}`}>
         <Heading level="1" size="xlarge">
-          {data ? data[0].nb[0].children[0].text : 'Sanity funker ikke'}
+          {/*       {     tittel ? tittel[0].nb[0].children[0].text : 'Sanity funker ikke'}
+           */}{' '}
         </Heading>
         <VeilederHilsen />
+        {/*         <p>{punktliste ? punktliste[0].nb[0].children[0].text :"Ingen punktliste her"}</p>
+         */}{' '}
       </div>
     </div>
   );
---
+++ app/typer/sanity/sanity.ts
@@ -0,0 +1,49 @@
+import { PortableTextBlock } from '@portabletext/types';
+
+export interface SanityDokument {
+  _createdAt: string;
+  _rev: string;
+  _type: string;
+  _id: string;
+  api_navn: ESanityApiKey;
+  visningsnavn: string;
+  nb: CustomSanityTyper.CUSTUM_BLOCK;
+  nn: CustomSanityTyper.CUSTUM_BLOCK;
+  en: CustomSanityTyper.CUSTUM_BLOCK;
+}
+
+export enum CustomSanityTyper {
+  CUSTUM_BLOCK = 'customBlock',
+}
+
+export enum ESanityApiKey {
+  TITTEL = 'tittelPaForside',
+  PUNKTLISTE = 'punktlistePaForside',
+}
+
+export type TextBlock = Record<ESanityApiKey, string> & {
+  api_navn: string;
+  [key: string]: unknown;
+};
+
+export interface IForsideTekstinnhold {
+  tittel: LocaleRecordString;
+  punktliste: LocaleRecordBlock;
+}
+
+//import { LocaleType } from '@navikt/familie-sprakvelger';
+export enum LocaleType {
+  en = 'en',
+  nb = 'nb',
+  nn = 'nn',
+}
+
+export type LocaleRecordString = Record<LocaleType, string> & {
+  api_navn: string;
+  [key: string]: unknown;
+};
+
+export type LocaleRecordBlock = Record<LocaleType, PortableTextBlock[]> & {
+  api_navn: string;
+  [key: string]: unknown;
+};
