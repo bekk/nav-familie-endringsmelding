cc086d19cdaf9d0ab6568b8a30a4ff1f418389b5
sedric <sedric.kvarnes@gmail.com>
2023-06-20 14:59:05 +0200

    la til en knapp til bekreftelse panelet

--- app/komponenter/confirmationPanel.tsx
+++ app/komponenter/confirmationPanel.tsx
@@ -1,17 +1,21 @@
 import { ConfirmationPanel } from '@navikt/ds-react';
 import { useState } from 'react';
+import { Button } from '@navikt/ds-react';
 
 const BekreftelsePanel = () => {
   const [read, setread] = useState(false);
   return (
-    <ConfirmationPanel
-      checked={read}
-      label="Ja, jeg samtykker."
-      onChange={() => setread(x => !x)}
-    >
-      For å komme videre må du gi oss lov til å hente inn og bruke opplysninger
-      om deg.
-    </ConfirmationPanel>
+    <div>
+      <ConfirmationPanel
+        checked={read}
+        label="Ja, jeg samtykker."
+        onChange={() => setread(x => !x)}
+      >
+        For å komme videre må du gi oss lov til å hente inn og bruke
+        opplysninger om deg.
+      </ConfirmationPanel>
+      <Button variant="secondary">Start</Button>
+    </div>
   );
 };
 
