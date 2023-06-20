56ae6ccb71ac2fd16c3059ef79b96fd4e01d62d4
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-20 15:44:28 +0200

    Skal legge inn oppførsel for Panel

--- app/komponenter/confirmationPanel.tsx
+++ app/komponenter/confirmationPanel.tsx
@@ -3,18 +3,23 @@ import { useState } from 'react';
 import { Button } from '@navikt/ds-react';
 
 const BekreftelsePanel = () => {
-  const [read, setread] = useState(false);
+  const [lest, settLest] = useState(false);
+  const [trykkVidere, settTrykkVidere] = useState(false);
+
   return (
     <div>
       <ConfirmationPanel
-        checked={read}
+        checked={lest}
         label="Ja, jeg samtykker."
-        onChange={() => setread(x => !x)}
+        onChange={() => settLest(x => !x)}
+        error={!lest && trykkVidere && 'Du må samtykke før du kan fortsette.'}
       >
         For å komme videre må du gi oss lov til å hente inn og bruke
         opplysninger om deg.
       </ConfirmationPanel>
-      <Button variant="secondary">Start</Button>
+      <Button variant="secondary" onClick={() => settTrykkVidere(true)}>
+        Start
+      </Button>
     </div>
   );
 };
