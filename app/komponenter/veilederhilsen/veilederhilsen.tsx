64c2c7eef4f4f1797d792c6a9d6edbd236c8a442
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-20 11:28:30 +0200

    skal legge opp mock-navn for veilederhilsen

--- app/komponenter/veilederhilsen/veilederhilsen.tsx
+++ app/komponenter/veilederhilsen/veilederhilsen.tsx
@@ -2,18 +2,32 @@ import { GuidePanel, Heading } from '@navikt/ds-react';
 import css from './veilederhilsen.module.css';
 import { SanityDokument } from '~/typer/sanity/sanity';
 import TekstBlokk from '../tekstBlokk/tekstBlokk';
+import søkerMock from '~/mock/søkerMock';
 
 interface VeilederHilsenProp {
   innhold: SanityDokument | undefined;
 }
 
+const hentSøkerFornavn = () => {
+  const søker = søkerMock;
+  //her skal vi fetche fra API
+  let fornavn: string = søker.visningsnavn.trim();
+  try {
+    fornavn = fornavn.split(' ')[0];
+  } catch (e) {
+    fornavn = '!';
+  }
+  return fornavn;
+};
+
 const VeilederHilsen: React.FC<VeilederHilsenProp> = ({
   innhold,
 }: VeilederHilsenProp) => {
+  const søkerFornavn = hentSøkerFornavn();
   return (
     <GuidePanel poster className={`${css.poster}`}>
       <Heading level="2" size="xlarge" className={`${css.tittelMargin}`}>
-        Hei [fornavn]
+        Hei {søkerFornavn}
       </Heading>
       <TekstBlokk tekstblokk={innhold} />
     </GuidePanel>
---
+++ app/mock/søkerMock.ts
@@ -0,0 +1,8 @@
+import { ISøker } from '~/typer/søker';
+
+const søkerMock: ISøker = {
+  ident: '12345678910',
+  visningsnavn: 'Kjetil mellomnavn Etternavn',
+};
+
+export default søkerMock;
