12a40b488b62e72c15233957d8ddffb13147f73c
elisetelje <elise.telje@bekk.no>
2023-06-21 12:32:26 +0200

    Skal vise en språkvelger med et globe icon i overskriften som endrer språken når man trykker på den

--- app/komponenter/språkvelger/språkvelger.tsx
+++ app/komponenter/språkvelger/språkvelger.tsx
@@ -13,19 +13,26 @@ export const Språkvelger = () => {
 
   return (
     <>
-      <GlobeIcon title="a11y-title" />
       <Select
-        label=""
-        hideLabel
+        label={<Label />}
         className={`${css.språkvelger}`}
         onChange={evt => {
           handleChange(evt);
         }}
       >
-        <option value={LocaleType.nb}> Bokmål</option>
-        <option value={LocaleType.nn}>Nynorsk</option>
+        <option value={LocaleType.nb}>Norsk (Bokmål)</option>
+        <option value={LocaleType.nn}>Norsk (Nynorsk)</option>
         <option value={LocaleType.en}>English</option>
       </Select>
     </>
+  );
+};
+
+const Label = () => {
+  return (
+    <div className={`${css.label}`}>
+      <GlobeIcon title="a11y-title" fontSize={'1.5rem'} />
+      <span> Språk/language </span>
+    </div>
   );
 };
