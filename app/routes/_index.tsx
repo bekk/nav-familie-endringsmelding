204b101c24f77aacd880934554f11df34d649665
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-22 15:34:23 +0200

    Midlertidige forsøk

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,14 +2,13 @@ import type { V2_MetaFunction } from '@remix-run/node';
 import css from './_index.module.css';
 import Spinner from '~/komponenter/Spinner';
 import VeilederHilsen from '~/komponenter/veilederhilsen/VeilederHilsen';
-import { ESanitySteg, SanityDokument } from '~/typer/sanity/sanity';
+import { ESanitySteg } from '~/typer/sanity/sanity';
 import TekstBlokk from '~/komponenter/tekstblokk/TekstBlokk';
 import { TypografiTyper } from '~/typer/typografi';
 import SamtykkePanel from '~/komponenter/SamtykkePanel';
 import { useTekster } from '~/hooks/contextHooks';
 import { Språkvelger } from '~/komponenter/språkvelger/språkvelger';
-import { useEffect, useState } from 'react';
-import { createClient } from '@sanity/client';
+/* import { useEffect, useState } from 'react'; */
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -21,39 +20,34 @@ export const meta: V2_MetaFunction = () => {
     },
   ];
 };
-
-const sanityKlient = createClient({
-  projectId: 'd8ycstqz',
-  dataset: 'production',
-  apiVersion: '2021-10-21',
-  useCdn: true,
-});
-
+/* 
 const API_URL_BACKEND: string =
   'http://localhost:8099/local/cookie?redirect=http://localhost:3000&issuerId=selvbetjening&audience=aud-localhost';
-
-export const loader = async () => {
-  const forsideTekst = await sanityKlient.fetch<SanityDokument[]>(
-    '*[steg == "FORSIDE"]',
-  );
-  return { forsideTekst };
-};
-
+ */
 export default function Index() {
   const tekster = useTekster(ESanitySteg.FORSIDE);
-
-  const [navn, settNavn] = useState('');
+  /* 
+  const [navn, settNavn] = useState(''); */
+  /* 
+  let headers = new Headers();
+  //headers.append('Content-Type', 'application/json');
+  headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+  headers.append('Access-Control-Allow-Headers', 'Content-Type');
+  //headers.append('Access-Control-Request-Method', 'GET');
 
   useEffect(() => {
     let data;
     const hentData = async () => {
-      data = await fetch(API_URL_BACKEND).catch(err => console.log);
+      data = await fetch(API_URL_BACKEND, {
+        headers: headers,
+      }).catch(err => console.log);
+      console.log('data: ', data);
     };
     hentData();
     if (data) {
       settNavn(data);
     }
-  }, [navn]);
+  }, [navn]); */
 
   return (
     <div className={`${css.fyllSide}`}>
