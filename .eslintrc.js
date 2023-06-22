08c4f62340fcc10e80b799f4d29bd78a9767b85a
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-22 11:29:43 +0200

    skal legge til esling for css

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     '@remix-run/eslint-config',
     '@remix-run/eslint-config/node',
     'prettier',
+    'plugin:css-modules/recommended',
   ],
-  plugins: ['unused-imports'],
+  plugins: ['unused-imports', 'css-modules'],
   rules: {
     '@typescript-eslint/consistent-type-imports': 'off',
     'no-unused-vars': 'off',
     'unused-imports/no-unused-imports': 'error',
+    'css-modules/no-unused-class': [2, { camelCase: true }],
+    'css-modules/no-undef-class': [2, { camelCase: true }],
   },
   ignorePatterns: ['!.*', 'dist', 'node_modules'],
 };
