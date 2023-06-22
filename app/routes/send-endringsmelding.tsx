eb3e3eae709ac227b418da7d282436b43782e8b9
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-22 10:53:50 +0200

    Fjerne MetaFunction fra ny side

--- app/routes/send-endringsmelding.tsx
+++ app/routes/send-endringsmelding.tsx
@@ -1,16 +1,4 @@
-import type { V2_MetaFunction } from '@remix-run/node';
 import React from 'react';
-
-export const meta: V2_MetaFunction = () => {
-  return [
-    { title: 'Familie endringsmelding' },
-    {
-      name: 'description',
-      content:
-        'Endringsmelding for barnetrygd, kontantstøtte og enslig forsørger',
-    },
-  ];
-};
 
 export default function SendEndringsmelding() {
   return <p>Send Endringsmelding</p>;
