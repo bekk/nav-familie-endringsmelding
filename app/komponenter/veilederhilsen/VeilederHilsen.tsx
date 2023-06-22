a677ceaf771e8b443a1f0b4fa3bf57b61054ca13
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-22 10:33:44 +0200

    Steg 2 i navnendring, skal endre til faktiske navn

--- app/komponenter/veilederhilsen/VeilederHilsen.tsx
+++ app/komponenter/veilederhilsen/VeilederHilsen.tsx
@@ -1,18 +1,15 @@
 import { GuidePanel } from '@navikt/ds-react';
 import css from './veilederhilsen.module.css';
 import { SanityDokument } from '~/typer/sanity/sanity';
-import TekstBlokk from '../tekstBlokk/tekstBlokk';
+import TekstBlokk from '../tekstblokk/TekstBlokk';
 import { TypografiTyper } from '~/typer/typografi';
 
-interface VeilederHilsenProp {
+interface Props {
   innhold: SanityDokument | undefined;
   hilsen: SanityDokument | undefined;
 }
 
-const VeilederHilsen: React.FC<VeilederHilsenProp> = ({
-  innhold,
-  hilsen,
-}: VeilederHilsenProp) => {
+const VeilederHilsen: React.FC<Props> = ({ innhold, hilsen }: Props) => {
   return (
     <GuidePanel poster className={`${css.poster}`}>
       <TekstBlokk
