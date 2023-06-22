6a4ccace45b4b41f5c890eb9d312e6da2a36f0ff
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-22 09:41:13 +0200

    Skal sette språk i html koden for å oppnå UU-kravet

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export default function App() {
   const [språk, settSpråk] = useState<LocaleType>(LocaleType.nb);
 
   return (
-    <html lang="en">
+    <html lang={språk}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
