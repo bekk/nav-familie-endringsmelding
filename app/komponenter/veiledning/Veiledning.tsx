47ece0156728135eed3e4f05a60f4abbe87b53fc
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-23 14:34:39 +0200

    Skal legge til veiledning komponent. Ikke ferdig.

---
+++ app/komponenter/veiledning/Veiledning.tsx
@@ -0,0 +1,17 @@
+import { GuidePanel } from '@navikt/ds-react';
+import TekstBlokk from '../tekstblokk/TekstBlokk';
+import { SanityDokument } from '~/typer/sanity/sanity';
+import { TypografiTyper } from '~/typer/typografi';
+
+interface Props {
+  hilsen: SanityDokument | undefined;
+}
+
+const Veiledning: React.FC<Props> = ({ hilsen }: Props) => {
+  // eslint-disable-line
+  return (
+    <GuidePanel>
+      <TekstBlokk tekstblokk={hilsen} typografi={TypografiTyper.BodyShort} />
+    </GuidePanel>
+  );
+};
