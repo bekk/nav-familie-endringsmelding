3fc9e0a58b875485c133332b684325154d499ad9
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-23 15:01:38 +0200

    Skal kunne skru av/på mock søker data med en boolean

--- app/utils/hentFraApi.ts
+++ app/utils/hentFraApi.ts
@@ -1,8 +1,19 @@
+import søkerMock from '~/mock/søkerMock';
 import { fetchWithToken } from '~/server/authorization';
 
 const API_URL_BACKEND: string = 'http://localhost:8099/api/oppslag/soker';
 
+const ER_MOCK: boolean = true;
+
 export const hentSøker = async (request: Request): Promise<any> => {
+  if (ER_MOCK) {
+    console.log('Mokker søker data!');
+    const mockInnhold = new Blob([JSON.stringify(søkerMock, null, 2)], {
+      type: 'application/json',
+    });
+    return new Response(mockInnhold);
+  }
+
   const søker = await fetchWithToken(request, API_URL_BACKEND);
   return søker;
 };
