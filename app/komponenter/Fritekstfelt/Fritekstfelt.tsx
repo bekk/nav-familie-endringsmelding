7117964c7657ba477ff0ffc1550fd9bfa62ffe85
elisetelje <elise.telje@bekk.no>
2023-06-26 16:35:11 +0200

    Skal vise et fritekstfelt som viser hvor mange karakterer man har igjen, og om det er skrevet inn tekst eller brukt spesialtegn

---
+++ app/komponenter/Fritekstfelt/Fritekstfelt.tsx
@@ -0,0 +1,78 @@
+import { Textarea } from '@navikt/ds-react';
+import css from './fritekstfelt.module.css';
+import { SanityDokument } from '~/typer/sanity/sanity';
+import TekstBlokk from '../tekstblokk/TekstBlokk';
+import { useSpråk } from '~/hooks/contextHooks';
+import { useState } from 'react';
+
+interface Props {
+  tittel: SanityDokument | undefined;
+  hjelpetekst: SanityDokument | undefined;
+  feilmeldingManglerTekst: SanityDokument | undefined;
+  feilmeldingManglerTegn: SanityDokument | undefined;
+}
+
+const Fritekstfelt: React.FC<Props> = ({
+  tittel,
+  hjelpetekst,
+  feilmeldingManglerTekst,
+  feilmeldingManglerTegn,
+}: Props) => {
+  const [språk] = useSpråk();
+  const specialCharsRegex = /[!@#$%^&*()?"{}|<>]/;
+  const [manglerTekst, settManglerTekst] = useState<boolean>(true);
+  const [brukerSpesialtegn, settBrukerSpesialtegn] = useState<boolean>(false);
+  const i18nInnhold = {
+    counterTooMuch: hentTekst(true),
+    counterLeft: hentTekst(false),
+  };
+
+  function hentTekst(tegnIgjen: boolean) {
+    switch (språk) {
+      case 'nb':
+        return tegnIgjen ? 'tegn igjen' : 'tegn for mye';
+      case 'nn':
+        return tegnIgjen ? 'teikn igjen' : 'teikn for mykje';
+      case 'en':
+        return tegnIgjen ? 'characters left' : 'characters too many';
+    }
+  }
+
+  const sjekkTekst = (handling: any) => {
+    const innholdVerdi = handling.target.value;
+
+    if (innholdVerdi.length == 0) {
+      settManglerTekst(true);
+    } else {
+      settManglerTekst(false);
+    }
+
+    if (innholdVerdi.match(specialCharsRegex)) {
+      settBrukerSpesialtegn(true);
+    } else {
+      settBrukerSpesialtegn(false);
+    }
+  };
+
+  return (
+    <Textarea
+      label={<TekstBlokk tekstblokk={tittel} />}
+      description={<TekstBlokk tekstblokk={hjelpetekst} />}
+      maxLength={10}
+      className={`${css.fritekstfelt}`}
+      i18n={i18nInnhold}
+      error={
+        manglerTekst ? (
+          <TekstBlokk tekstblokk={feilmeldingManglerTekst} />
+        ) : (
+          brukerSpesialtegn && (
+            <TekstBlokk tekstblokk={feilmeldingManglerTegn} />
+          )
+        )
+      }
+      onInput={event => sjekkTekst(event)}
+    />
+  );
+};
+
+export default Fritekstfelt;
--- app/routes/send-endringsmelding.tsx
+++ app/routes/send-endringsmelding.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import TekstBlokk from '~/komponenter/tekstblokk/TekstBlokk';
+
 import { ESanitySteg } from '~/typer/sanity/sanity';
 import { TypografiTyper } from '~/typer/typografi';
 import { useTekster } from '~/hooks/contextHooks';
 import InnholdKonteiner from '~/komponenter/innholdkonteiner/InnholdKonteiner';
+import Fritekstfelt from '~/komponenter/Fritekstfelt/Fritekstfelt';
 
 export default function SendEndringsmelding() {
   const tekster = useTekster(ESanitySteg.SEND_ENDRINGER);
 
   return (
     <InnholdKonteiner>
-      <TekstBlokk
-        tekstblokk={tekster.overskrift}
-        typografi={TypografiTyper.StegHeadingH1}
-      />
+      <>
+        <TekstBlokk
+          tekstblokk={tekster.overskrift}
+          typografi={TypografiTyper.StegHeadingH1}
+        />
+        <Fritekstfelt
+          tittel={tekster.fritekstfeltTittel}
+          hjelpetekst={tekster.fritekstfeltHjelpetekst}
+          feilmeldingManglerTekst={tekster.fritekstfeltFeilmeldingTekst}
+          feilmeldingManglerTegn={tekster.fritekstfeltFeilmeldingTegn}
+        />
+      </>
     </InnholdKonteiner>
   );
 }
