566eda84ea3e5a496ee51b0e92f61d5d2dccb834
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-26 09:38:45 +0200

    Skal legge til switchcase med riktig url

--- app/utils/hentFraApi.ts
+++ app/utils/hentFraApi.ts
@@ -3,12 +3,18 @@ import { fetchWithToken } from '~/server/authorization';
 import { EMiljø } from '~/typer/miljø';
 import { ISøker } from '~/typer/søker';
 
-const API_URL_BACKEND: string = 'http://localhost:8099/api/oppslag/soker';
+const STI: string = '/api/oppslag/soker';
+const LOCAL_URL_BACKEND: string = 'http://localhost:8099' + STI;
+const API_URL_BACKEND: string =
+  'https://nav-familie-endringsmelding-api.fly.dev/' + STI;
 
 export const hentSøker = async (request: Request): Promise<ISøker> => {
-  if (process.env.ENV == EMiljø.LOKAL) {
-    return Promise.resolve(søkerMock);
+  switch (process.env.ENV) {
+    case EMiljø.LOKAL:
+      return (await fetchWithToken(request, LOCAL_URL_BACKEND)).json();
+    case EMiljø.PRODUKSJON:
+      return (await fetchWithToken(request, API_URL_BACKEND)).json();
+    default:
+      return Promise.resolve(søkerMock);
   }
-
-  return (await fetchWithToken(request, API_URL_BACKEND)).json();
 };
