22431ac623bd83b1272481f565a88a73f52bf3d6
Kjetil Indrehus <kjetil.indrehus@bekk.no>
2023-06-26 11:10:02 +0200

    Skal bruke InnholdKonteiner som wrapper for å få styling riktig

---
+++ app/komponenter/innholdkonteiner/InnholdKonteiner.tsx
@@ -0,0 +1,15 @@
+import css from './innholdKonteiner.module.css';
+
+interface Props {
+  children?: JSX.Element;
+}
+
+const InnholdKonteiner: React.FC<Props> = ({ children }) => {
+  return (
+    <div className={`${css.fyllSide}`}>
+      <div className={`${css.innholdKonteiner}`}>{children}</div>
+    </div>
+  );
+};
+
+export default InnholdKonteiner;
