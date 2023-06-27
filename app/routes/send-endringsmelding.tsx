395352b6a745e8e37d83f48cbfa61de145168c5c
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-06-27 10:44:51 +0200

    Legge til knapper i side 2

--- app/routes/send-endringsmelding.tsx
+++ app/routes/send-endringsmelding.tsx
@@ -5,6 +5,7 @@ import { TypografiTyper } from '~/typer/typografi';
 import { useTekster } from '~/hooks/contextHooks';
 import InnholdKonteiner from '~/komponenter/innholdkonteiner/InnholdKonteiner';
 import VidereKnapp from '~/komponenter/VidereKnapp';
+import css from './send-endringsmelding.module.css';
 
 export default function SendEndringsmelding() {
   const tekster = useTekster(ESanitySteg.SEND_ENDRINGER);
@@ -16,12 +17,19 @@ export default function SendEndringsmelding() {
           tekstblokk={tekster.overskrift}
           typografi={TypografiTyper.StegHeadingH1}
         />
-        <VidereKnapp
-          kanGåVidere={true} //skal denne alltid være true, eller skal man legge på en state, slik som i index?
-          nesteSteg={ESanitySteg.FORSIDE}
-          tekstPåKnapp={'Tilbake'}
-          //gjorde knappeTrykkUtenSamtykke optional, denne må vi vurdere om vi trenger eller ikke ut ifra om det er noe som må "verifiseres" før en kan navigere videre (slik som på forsiden)
-        />
+        <div className={`${css.navigeringsKnapper}`}>
+          <VidereKnapp
+            kanGåVidere={true} //skal denne alltid være true, eller skal man legge på en state, slik som i index?
+            nesteSteg={ESanitySteg.FORSIDE}
+            tekstPåKnapp={'Tilbake'}
+            //gjorde knappeTrykkUtenSamtykke optional, denne må vi vurdere om vi trenger eller ikke ut ifra om det er noe som må "verifiseres" før en kan navigere videre (slik som på forsiden)
+          />
+          <VidereKnapp
+            kanGåVidere={false}
+            nesteSteg={ESanitySteg.FORSIDE}
+            tekstPåKnapp={'Gå videre'}
+          />
+        </div>
       </>
     </InnholdKonteiner>
   );
