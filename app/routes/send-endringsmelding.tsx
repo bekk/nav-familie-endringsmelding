2fd4d5f8097a6b5df73dcc01908034a3d8c65fe0
Kjetil Indrehus <66110094+KjetilIN@users.noreply.github.com>
2023-06-28 12:06:03 +0200

    Veiledning Steg 1 (#30)
    ### Hvorfor er dette nødvendig? ✨
    
    Skal vise følgende hilsen på steg 1. 
    Tekstene skal bli hentet fra Sanity; 
    
    
    ![image](https://github.com/bekk/nav-familie-endringsmelding/assets/66110094/2502773f-82ef-4c24-9c9e-2cf92849dbfd)
    
    [Lenke til trello
    kort](https://trello.com/c/Cqv9kj5P/29-veiledningstekst-side-2)
    
    ### Hvordan er det løst? 🧠
    
    Lagde et eget komponent for vise `Veiledning` på steg 1. Har ikke blitt
    stylet pga ønsker å bruke komponent fra #27
    Henter tekster fra Sanity i alle språk. Må typesterke steg 1, når
    forsiden er typesterket. ☀️ Nytt kort for typesterking [i steg 1
    her.](https://trello.com/c/D1eD5W0O/70-sterkere-typing-steg-1)
    
    Slik er det per nå:
    <img width="760" alt="Screenshot 2023-06-27 at 09 28 58"
    src="https://github.com/bekk/nav-familie-endringsmelding/assets/66110094/b43885a3-ab92-489c-ba01-f436dbb797cd">

--- app/routes/send-endringsmelding.tsx
+++ app/routes/send-endringsmelding.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import TekstBlokk from '~/komponenter/tekstblokk/TekstBlokk';
-import { ESanitySteg } from '~/typer/sanity/sanity';
-import { TypografiTyper } from '~/typer/typografi';
 import { useTekster } from '~/hooks/contextHooks';
+import { ESanitySteg } from '~/typer/sanity/sanity';
+import TekstBlokk from '~/komponenter/tekstblokk/TekstBlokk';
+import { TypografiTyper } from '~/typer/typografi';
 import InnholdKonteiner from '~/komponenter/innholdkonteiner/InnholdKonteiner';
+import Veiledning from '~/komponenter/veiledning/Veiledning';
 
 export default function SendEndringsmelding() {
   const tekster = useTekster(ESanitySteg.SEND_ENDRINGER);
@@ -14,6 +15,7 @@ export default function SendEndringsmelding() {
         tekstblokk={tekster.overskrift}
         typografi={TypografiTyper.StegHeadingH1}
       />
+      <Veiledning hilsen={tekster.veilederInnhold} />
     </InnholdKonteiner>
   );
 }
