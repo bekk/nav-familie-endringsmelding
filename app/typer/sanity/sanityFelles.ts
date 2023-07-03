1d93297e4fefb305b7e9aaf85201838f1d3890fd
sedriccc <31205453+sedriccc@users.noreply.github.com>
2023-07-03 09:50:23 +0200

    Hent tekst til startknapp på forside fra sanity (#44)
    ### Hvorfor er dette nødvendig? ✨
    
    Teksten er hentet fra sanity, så den enkelt kan endres
    [Lenke til trello
    kort](https://trello.com/c/Wj8hMNq1/75-hente-tekst-på-knapper-fra-sanity)
    
    ### Hvordan er det løst? 🧠
    Jeg endret fra "start" i knappen til å hente tekst fra sanity, ved bruk
    av tekstblokk

--- app/typer/sanity/sanityFelles.ts
+++ app/typer/sanity/sanityFelles.ts
@@ -4,6 +4,7 @@ export enum EApiKeysFelles {
   KNAPP_TILBAKE = 'knappTilbake',
   KNAPP_SEND_ENDRINGER = 'knappSendEndringer',
   BANNER = 'bannerTekst',
+  KNAPP_START = 'knappStart',
 }
 
 export type IFellesTekstinnhold = Record<EApiKeysFelles, SanityDokument>;
