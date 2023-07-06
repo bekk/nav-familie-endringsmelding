79d8ef8dac60e6c81888e25a663af79aa2a269fc
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-07-06 13:28:11 +0200

    la til test

--- cypress/e2e/steg1.cy.ts
+++ cypress/e2e/steg1.cy.ts
@@ -2,12 +2,22 @@ describe('Steg1-test', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/send-endringsmelding');
   });
-  it('Tekst laster fra Sanity', () => {
+  it('Sjekker at tekst laster fra Sanity', () => {
     cy.get('.navds-heading--small').contains('Send endringer');
   });
-  it('Tekst i veiledning', () => {
+  it('Finner tekst i veiledning', () => {
     cy.get(`[data-testid='veiledningSteg1']`).contains(
       'Veiledning på endringsmelding',
     );
+  });
+  it('Sjekker at stegindikator har rett antall steg', () => {
+    cy.get('.navds-stepper').children().should('have.length', 2);
+  });
+  //denne går ikke:
+  /*   it('Sjekker at stegindikator har rattributt activeStep, () => {
+    cy.get('.navds-stepper').invoke('attr', 'activeStep').should('exist');
+  }); */
+  it('Stegindikator er på rett steg', () => {
+    cy.get('.navds-stepper__step').eq(1).contains('2');
   });
 });
