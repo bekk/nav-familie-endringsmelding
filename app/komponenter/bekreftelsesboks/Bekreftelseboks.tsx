716b946831c9aa8eb18c5569f18786b7a6b8ebfa
sunnivabjerke <bjerke.sunniva@gmail.com>
2023-07-11 15:24:13 +0200

    Skal legge inn flettefelt for innsendt tid, i tillegg til formattering av dato

--- app/komponenter/bekreftelsesboks/Bekreftelseboks.tsx
+++ app/komponenter/bekreftelsesboks/Bekreftelseboks.tsx
@@ -2,21 +2,19 @@ import { Alert } from '@navikt/ds-react';
 import { useTekster } from '~/hooks/contextHooks';
 import { ESanityMappe } from '~/typer/felles';
 import TekstBlokk from '../tekstblokk/TekstBlokk';
+import { formaterDato } from '~/utils/formaterDato';
 
 const BekreftelseBoks = () => {
   const { bekreftelseBoksInnhold } = useTekster(ESanityMappe.KVITTERING);
+
+  //TODO: Dato-string skal byttes ut med dato hentet fra api
+  const innsendingsTid = formaterDato('2023-07-06T13:44:00.00000');
 
   return (
     <Alert variant="success">
       <TekstBlokk
         tekstblokk={bekreftelseBoksInnhold}
-        /*  
-        i bekreftelseBoksInnhold må det legges opp flettefelt for dato og tid hentet fra backend
-        mulig det også bør legges til en metode for å gjøre om dato til en string på riktig format
-        nåværende format i send-data-backend: Mottatt dato: 2023-07-11T13:41:28.15382
-        vi ønsker Mottatt dato: tt:mm, dd:mm:åå 
-        */
-        /* flettefelter={tid} */
+        flettefelter={{ innsendtTid: innsendingsTid }}
       />
     </Alert>
   );
