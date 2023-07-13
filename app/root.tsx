c3d72a66d4b89576e0cb8f2e72598dfee08f03a7
elisetelje <elise.telje@bekk.no>
2023-07-13 12:25:46 +0200

    skal fikse kansellert get som ble kjørt etter post av endringsmelding

--- app/root.tsx
+++ app/root.tsx
@@ -13,6 +13,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  ShouldRevalidateFunction,
   useLoaderData,
 } from '@remix-run/react';
 import { hentSanityData } from './server/hentSanityData.server';
@@ -39,6 +40,16 @@ export const links: LinksFunction = () => [
   },
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
 ];
+
+export const shouldRevalidate: ShouldRevalidateFunction = ({
+  formMethod,
+  defaultShouldRevalidate,
+}) => {
+  if (formMethod === 'POST') {
+    return false;
+  }
+  return defaultShouldRevalidate;
+};
 
 export const loader: LoaderFunction = async ({ request }: LoaderArgs) => {
   const session = await getSession(request.headers.get('Cookie'));
