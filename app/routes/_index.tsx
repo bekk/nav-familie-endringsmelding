8a44a24244f1de5ac0d6ec692cecee20bb472da4
elisetelje <elise.telje@bekk.no>
2023-07-17 14:30:44 +0200

    skal bruke hentPath framfor å skrive pathen direkte

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,9 @@
 import { LinkIcon } from '@navikt/aksel-icons';
 import type { V2_MetaFunction } from '@remix-run/node';
 
+import { EYtelse } from '~/typer/ytelse';
+import { hentPathForYtelse } from '~/utils/hentPath';
+
 import css from './_index.module.css';
 
 export const meta: V2_MetaFunction = () => {
@@ -16,11 +19,17 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div className={`${css.linkKonteiner}`}>
-      <a href="ba" className={`${css.ytelseLink}`}>
+      <a
+        href={hentPathForYtelse(EYtelse.BARNETRYGD)}
+        className={`${css.ytelseLink}`}
+      >
         <LinkIcon title="link-icon" fontSize="1.5rem" />
         <p>Barnetrygd</p>
       </a>
-      <a href="ks" className={`${css.ytelseLink}`}>
+      <a
+        href={hentPathForYtelse(EYtelse.KONTANTSTØTTE)}
+        className={`${css.ytelseLink}`}
+      >
         <LinkIcon title="link-icon" fontSize="1.5rem" />
         <p>Kontantstøtte</p>
       </a>
