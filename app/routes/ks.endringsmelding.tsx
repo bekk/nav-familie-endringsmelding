b1ae41a07982e0d190221fff9f48f52de78e788d
elisetelje <elise.telje@bekk.no>
2023-07-18 15:02:25 +0200

    skal legge til action i ks.endringsmelding for å sende endringsmelding

--- app/routes/ks.endringsmelding.tsx
+++ app/routes/ks.endringsmelding.tsx
@@ -1,4 +1,21 @@
+import { ActionArgs } from '@remix-run/node';
+
+import { sendEndringsmelding } from '~/server/sendEndringsmelding.server';
+import { getSession } from '~/sessions';
 import SendEndringSide from '~/sider/SendEndring';
+import { RESPONSE_STATUS_FEIL } from '~/typer/response';
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const endringsmelding = formData.get('endringsmelding') as string;
+  return await sendEndringsmelding(
+    endringsmelding,
+    await getSession(request.headers.get('Cookie')),
+  ).then(response => {
+    if (response.ok) return response.json();
+    return { text: RESPONSE_STATUS_FEIL };
+  });
+}
 
 export default function KSEndringsmelding() {
   return <SendEndringSide />;
