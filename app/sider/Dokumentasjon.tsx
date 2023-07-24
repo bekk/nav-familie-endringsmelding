131b082580e6e82308bb994ae56dac832bbcbd05
sedric <sedric.kvarnes@gmail.com>
2023-07-24 12:55:42 +0200

    lagt til overskrift på dokumentasjon siden

--- app/sider/Dokumentasjon.tsx
+++ app/sider/Dokumentasjon.tsx
@@ -1,11 +1,20 @@
+import { useTekster } from '~/hooks/contextHooks';
 import HovedInnhold from '~/komponenter/hovedInnhold/HovedInnhold';
 import StegIndikator from '~/komponenter/stegindikator/StegIndikator';
+import TekstBlokk from '~/komponenter/tekstblokk/TekstBlokk';
+import { ESanityMappe } from '~/typer/felles';
+import { ETypografiTyper } from '~/typer/typografi';
 
 export default function DokumentasjonSide() {
+  const tekster = useTekster(ESanityMappe.DOKUMENTASJON);
   return (
     <HovedInnhold /*måHaBekreftetSamtykke*/>
       <StegIndikator nåværendeSteg={2} />
-      <h1>Dokumentasjonsopplastning her</h1>
+      <TekstBlokk
+        tekstblokk={tekster.dokumentasjonOverskrift}
+        typografi={ETypografiTyper.STEG_HEADING_SMALL_H1}
+        dataTestid="overskriftDokumentering"
+      />
     </HovedInnhold>
   );
 }
