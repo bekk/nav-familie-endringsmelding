d5ae84b9d3a219d90b3ef1cf8b73e7de5f59a45c
elisetelje <elise.telje@bekk.no>
2023-07-24 16:44:31 +0200

    skal flytte action fra endringsmeldingssiden til dokumentasjonssiden

--- app/routes/ba.dokumentasjon.tsx
+++ app/routes/ba.dokumentasjon.tsx
@@ -1,4 +1,11 @@
+import { ActionArgs } from '@remix-run/node';
+
+import sendEndringAction from '~/server/sendEndringAction.server';
 import DokumentasjonSide from '~/sider/Dokumentasjon';
+
+export async function action({ request }: ActionArgs) {
+  return sendEndringAction(request);
+}
 
 export default function BADokumentasjon() {
   return <DokumentasjonSide />;
