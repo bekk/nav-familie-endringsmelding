a32b6747a6c752251be19af2e2637c3c7de5945c
sarahjelle <sara.hjelle@bekk.no>
2023-07-26 09:26:57 +0200

    Styling. Høyrestill lenke for ekspanderbart panel

--- app/komponenter/bildeScanningGuide/TaGodtBilde.tsx
+++ app/komponenter/bildeScanningGuide/TaGodtBilde.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 
 import GodtBildeToggleLink from './GodtBildeToggleLink';
+import css from './taGodtBilde.module.css';
 import TaGodtBildeInnhold from './TaGodtBildeInnhold';
 
 const TaGodtBildeInfo = () => {
   const [erÅpen, settErÅpen] = useState<boolean>(false);
   return (
-    <div>
+    <div className={`${css.kontainer}`}>
       <GodtBildeToggleLink
         vedToggle={() => settErÅpen(!erÅpen)}
         erÅpen={erÅpen}
